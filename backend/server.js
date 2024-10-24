100952da12630ea20946d48a1cf1aaf084dd2981
mtmenji <mtmenji@gmail.com>
2024-10-24 17:04:56 -0600

    reseting attemps

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,6 @@
 require('dotenv').config()
 const express = require('express')
 const mongoose = require('mongoose')
-const path = require('path')
 
 const expenseRoutes = require('./routes/expenses')
 const userRoutes = require('./routes/user')
@@ -12,20 +11,14 @@ const app = express()
 //Middleware
 app.use(express.json())
 
-// app.use((req, res, next) => {
-//     console.log(req.path, req.method)
-//     next()
-// })
+app.use((req, res, next) => {
+    console.log(req.path, req.method)
+    next()
+})
 
 //Routes
 app.use('/expenses',expenseRoutes)
 app.use('/user',userRoutes)
-
-//Host
-app.use(express.static(path.join('../frontend/build')))
-app.get('*', (req, res) =>
-    res.sendFile(path.join('../frontend/build/index.html'))
-);
 
 //Database Connection
 mongoose.connect(process.env.MONGO_URI)
