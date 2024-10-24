724727a6b588801ea0b27c0780a52d97b2b7312b
mtmenji <mtmenji@gmail.com>
2024-10-24 17:00:21 -0600

    attempt to fix

--- backend/server.js
+++ backend/server.js
@@ -12,10 +12,10 @@ const app = express()
 //Middleware
 app.use(express.json())
 
-app.use((req, res, next) => {
-    console.log(req.path, req.method)
-    next()
-})
+// app.use((req, res, next) => {
+//     console.log(req.path, req.method)
+//     next()
+// })
 
 //Routes
 app.use('/expenses',expenseRoutes)
