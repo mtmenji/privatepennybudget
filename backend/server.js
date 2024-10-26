def1ccdf292b6a3be66638d14ccaab6dfbde8a53
mtmenji <mtmenji@gmail.com>
2024-10-26 05:40:46 -0600

    defining path

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 require('dotenv').config()
 const express = require('express')
 const mongoose = require('mongoose')
+const path = require('path')
 
 const expenseRoutes = require('./routes/expenses')
 const userRoutes = require('./routes/user')
