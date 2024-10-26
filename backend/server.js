de77f05525fab0c1c9943901e11dbed04b254866
mtmenji <mtmenji@gmail.com>
2024-10-26 05:24:51 -0600

    build files and host update

--- backend/server.js
+++ backend/server.js
@@ -20,6 +20,12 @@ app.use((req, res, next) => {
 app.use('/expenses',expenseRoutes)
 app.use('/user',userRoutes)
 
+//Host
+app.use(express.static(path.join(__dirname, '../frontend/build')))
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '../frontend/build/index.html'))
+});
+
 //Database Connection
 mongoose.connect(process.env.MONGO_URI)
     .then(()=> {
