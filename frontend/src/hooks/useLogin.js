7697277a046ed035073bce5585a0b472f60e4cbe
mtmenji <mtmenji@gmail.com>
2024-11-26 15:17:44 -0500

    deployment testing

--- frontend/src/hooks/useLogin.js
+++ frontend/src/hooks/useLogin.js
@@ -10,7 +10,7 @@ export const useLogin = () => {
         setIsLoading(true)
         setError(null)
 
-        const response = await fetch('/user/login', {
+        const response = await fetch('https://budgetbuddy-backend-hhs9.onrender.com/user/login', {
             method: 'POST',
             headers: {'Content-Type': 'application/json'},
             body: JSON.stringify({email, password})
