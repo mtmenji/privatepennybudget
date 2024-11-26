7abe151fc97c970618540f5f0c43e09dfb94399c
mtmenji <mtmenji@gmail.com>
2024-11-26 10:49:29 -0500

    testing backend routes

--- frontend/src/hooks/useRegister.js
+++ frontend/src/hooks/useRegister.js
@@ -27,7 +27,7 @@ export const useRegister = () => {
                 theme = 'theme-parchment';
         }
 
-        const response = await fetch('/user/register', {
+        const response = await fetch('https://budgetbuddy-backend-hhs9.onrender.com/user/register', {
             method: 'POST',
             headers: {'Content-Type': 'application/json'},
             body: JSON.stringify({email, password, nickname, theme})
