78b6b40510084c10609a9a9a3063a34b871af999
mtmenji <mtmenji@gmail.com>
2024-12-09 22:51:50 -0500

    semi-mobile responsiveness

--- frontend/src/pages/Dashboard.js
+++ frontend/src/pages/Dashboard.js
@@ -17,20 +17,20 @@ const Dashboard = () => {
     };
 
     return (
-        <div className='w-full flex'>
-            <div className='w-4/12'>
+        <div className='w-full flex flex-col md:flex-row'>
+            <div className='w-full md:w-4/12'>
                 <Selection onSelectionChange={handleSelectionChange} />
                 <hr className="border-t-4 border-dark1 rounded-2xl mx-2 my-4" />
                 <PaymentReminders/>
             </div>
-            <div className='w-5/12 flex justify-center items-center'>
+            <div className='w-full md:w-5/12 flex justify-center items-center'>
                 {selectedBudgetId ? (
                     <Gallery selectedBudgetId={selectedBudgetId} selectedMonth={selectedMonth} selectedYear={selectedYear} />
                 ) : (
                     <p>Please select a budget to view your reports.</p>
                 )}
             </div>
-            <div className='w-3/12'>
+            <div className='w-full md:w-3/12'>
                 <Goals/>
             </div>
         </div>
