6169123685eab8e886966869f2d8258617aef8fb
mtmenji <mtmenji@gmail.com>
2024-12-14 13:22:24 -0500

    updating to prod testing v2

--- frontend/src/hooks/useLogin.js
+++ frontend/src/hooks/useLogin.js
@@ -10,7 +10,7 @@ export const useLogin = () => {
         setIsLoading(true)
         setError(null)
 
-        const response = await fetch('/user/login', {
+        const response = await fetch('https://privatepennybudgettest-backend.onrender.com/user/login', {
             method: 'POST',
             headers: {'Content-Type': 'application/json'},
             body: JSON.stringify({email, password})
